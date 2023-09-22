29dc3574fa0596ad8c0c17b3551c31fca2414648
Hamid Bourja <bourjamthb@gmail.com>
2023-09-22 18:37:41 +0100

    feat: add create user (Inscription)

--- modules/User.php
+++ modules/User.php
@@ -15,4 +15,40 @@ class User
   {
     $this->connection = $db;
   }
+
+  public function create()
+  {
+    $sql = 'INSERT INTO users SET 
+    first_name = :first_name,
+    last_name = :last_name ,
+    email = :email,
+    address = :address,
+    phone = :phone,
+    password = :password';
+
+    $stmt = $this->connection->prepare($sql);
+
+    $this->first_name = htmlspecialchars(strip_tags($this->first_name));
+    $this->last_name = htmlspecialchars(strip_tags($this->last_name));
+    $this->email = htmlspecialchars(strip_tags($this->email));
+    $this->address = htmlspecialchars(strip_tags($this->address));
+    $this->phone = htmlspecialchars(strip_tags($this->phone));
+    $this->password = htmlspecialchars(strip_tags($this->password));
+
+    $stmt->bindParam(':first_name', $this->first_name);
+    $stmt->bindParam(':last_name', $this->last_name);
+    $stmt->bindParam(':email', $this->email);
+    $stmt->bindParam(':address', $this->address);
+    $stmt->bindParam(':phone', $this->phone);
+
+    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+
+    $stmt->bindParam(':password', $password_hash);
+
+    if ($stmt->execute()) {
+      return true;
+    }
+
+    return false;
+  }
 }
