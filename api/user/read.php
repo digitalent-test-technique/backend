d994ff00fad41beeb4c7e3db4507fe674e573094
Hamid Bourja <bourjamthb@gmail.com>
2023-09-22 19:31:32 +0100

    feat: add `read` api route

---
+++ api/user/read.php
@@ -0,0 +1,41 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../../config/Database.php';
+include_once '../../modules/User.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new User($db);
+$user->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+$result = $user->getUser();
+$num = $result->rowCount();
+
+if ($num > 0) {
+  $user_arr = array();
+  $user_arr['data'] = array();
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
+    $user_arr['data'] = array(
+      'id'          => $id,
+      'first_name'  => $first_name,
+      'last_name'   => $last_name,
+      'email'       => $email,
+      'address'     => $address,
+      'phone'       => $phone,
+    );
+  }
+  http_response_code(200);
+  echo json_encode($user_arr);
+} else {
+  http_response_code(400);
+  echo json_encode(array(
+    "message" => "No user found"
+  ));
+}
