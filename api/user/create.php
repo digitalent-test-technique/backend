a25511efd9518bbcecd7b3e25ebb8528e0f7ce09
Hamid Bourja <bourjamthb@gmail.com>
2023-09-23 00:32:58 +0100

    fix: handle the preflight request

--- api/user/create.php
+++ api/user/create.php
@@ -1,13 +1,15 @@
 <?php
 
-/**
- * use `*` for now
- */
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 include_once '../../config/Database.php';
 include_once '../../modules/User.php';
